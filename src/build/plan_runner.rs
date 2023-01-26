53c2f3165423d0af1451f70b37005c5cd9cfb0de
geralddejong <geralddejong@gmail.com>
2023-01-26 19:51:28 +0000

    Headless hug experiment

--- src/build/plan_runner.rs
+++ src/build/plan_runner.rs
@@ -27,7 +27,7 @@ pub struct PlanRunner {
 impl Default for PlanRunner {
     fn default() -> Self {
         Self {
-            growth: Growth::new(fabric_plan("Halo by Crane")),
+            growth: Growth::new(fabric_plan("Headless Hug")),
             stage: Empty,
         }
     }
