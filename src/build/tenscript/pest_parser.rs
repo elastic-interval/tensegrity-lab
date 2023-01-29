c7458d6da5f9e776fbf5d48773e873510d3336cf
geralddejong <geralddejong@gmail.com>
2023-01-29 09:10:45 +0000

    Seed added

--- src/build/tenscript/pest_parser.rs
+++ src/build/tenscript/pest_parser.rs
@@ -3,7 +3,7 @@ use pest::error::Error;
 use pest::iterators::Pair;
 use pest::Parser;
 use pest_derive::Parser;
-use crate::build::tenscript::{FabricPlan, SurfaceCharacterSpec};
+use crate::build::tenscript::{BuildPhase, FabricPlan, Seed, SurfaceCharacterSpec};
 
 #[derive(Parser)]
 #[grammar = "build/tenscript/tenscript.pest"] // relative to src
@@ -28,13 +28,39 @@ fn fabric_plan(fabric_plan_pair: Pair<Rule>) -> Result<FabricPlan, ParseError> {
                         _ => unreachable!()
                     }
                 );
-                dbg!(plan.surface);
             }
-            _ => unreachable!(),
+            Rule::build => {
+                plan.build_phase = build(pair)?;
+            }
+            _ => unreachable!("fabric plan"),
         }
     }
     Ok(plan)
 }
+
+fn build(build_phase_pair: Pair<Rule>)-> Result<BuildPhase, ParseError> {
+    let mut phase = BuildPhase::default();
+    for pair in build_phase_pair.into_inner() {
+        match pair.as_rule() {
+            Rule::seed => {
+                phase.seed =
+                    match pair.into_inner().next().unwrap().as_str() {
+                        ":left-right" => Seed::LeftRight,
+                        ":right-left" => Seed::RightLeft,
+                        ":left" => Seed::Left,
+                        ":right" => Seed::Right,
+                        _ => unreachable!()
+                    };
+            }
+            _ => unreachable!("build phase: {:?}", pair.as_rule()),
+        }
+    }
+    Ok(phase)
+}
+
+// fn seed(_seed_pair: Pair<Rule>)-> Result<Seed, ParseError> {
+//     Ok(Seed::default())
+// }
 
 fn parse(source: &str) -> Result<FabricPlan, ParseError> {
     let mut pairs = PestParser::parse(Rule::fabric_plan, source)
@@ -56,7 +82,10 @@ mod tests {
                 continue;
             }
             match parse(code) {
-                Ok(_) => println!("[{name}] Good plan!"),
+                Ok(plan) => {
+                    println!("[{name}] Good plan!");
+                    dbg!(plan);
+                },
                 Err(ParseError::PestError(error)) => panic!("[{name}] Error: {error}"),
                 Err(error) => panic!("[{name}] Error: {error:?}"),
             }
