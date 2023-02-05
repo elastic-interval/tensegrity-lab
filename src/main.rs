6c578b4233f7c991f4e3d34b123d8b3e4e693d07
Mitchell Justin <8631205+mitchelljustin@users.noreply.github.com>
2023-02-05 11:37:50 +0000

    Generate command line option for prototype from enum BrickType

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
 use clap::Parser;
 
-use tensegrity_lab::build::brick::BrickName::LeftTwist;
+use tensegrity_lab::build::brick::BrickName;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Name of the prototype to settle and capture
-    #[arg(long)]
-    prototype: Option<String>,
+    #[arg(long, value_enum)]
+    prototype: Option<BrickName>,
 }
 
 fn main() {
     let args = Args::parse();
-    let brick_name = args.prototype.map(|name|
-        match name.as_str() {
-            "LeftTwist" => LeftTwist,
-            _ => panic!("no such prototype: {name}")
-        }
-    );
+    let brick_name = args.prototype;
     tensegrity_lab::application::run_with(brick_name);
 }
