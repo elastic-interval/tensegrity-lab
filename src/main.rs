21df2367c86d6264984b6107d1f0196410aabceb
Mitchell Justin <8631205+mitchelljustin@users.noreply.github.com>
2023-02-11 18:16:34 +0000

    Make prototype CLI arg a usize to parse automatically

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,10 @@ use clap::Parser;
 struct Args {
     /// Name of the prototype to settle and capture
     #[arg(long)]
-    prototype: Option<String>,
+    prototype: Option<usize>,
 }
 
 fn main() {
     let Args { prototype } = Args::parse();
-    let brick_index = prototype.map(|s| s.parse().expect("argument is a brick index"));
-    tensegrity_lab::application::run_with(brick_index);
+    tensegrity_lab::application::run_with(prototype);
 }
