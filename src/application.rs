ef13221b5dc9c10d7de759838228feef7e978286
Mitchell Justin <8631205+mitchelljustin@users.noreply.github.com>
2023-02-12 14:56:16 +0000

    Move almost all logic into application.rs, move main run() function into main.rs.

--- src/application.rs
+++ src/application.rs
@@ -2,18 +2,11 @@ use std::{fs, iter};
 use std::time::SystemTime;
 
 use iced_wgpu::wgpu;
-#[allow(unused_imports)]
-use log::info;
 use winit::{
     event::*,
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
 };
 use winit::dpi::PhysicalSize;
 use winit::window::Window;
-
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::*;
 
 use crate::build::tenscript::{FabricPlan, FaceAlias, Spin};
 use crate::camera::Target::{FabricMidpoint, Hold, Origin, SelectedFace};
@@ -24,23 +17,107 @@ use crate::crucible::Crucible;
 use crate::graphics::GraphicsWindow;
 use crate::scene::Scene;
 
-struct Application {
+pub struct Application {
     graphics: GraphicsWindow,
     scene: Scene,
     gui: GUI,
     crucible: Crucible,
+    library_modified: SystemTime,
+    fabric_plan_name: Option<String>,
 }
 
 impl Application {
-    fn new(graphics: GraphicsWindow, window: &Window) -> Application {
+    pub fn capture_prototype(&mut self, prototype: usize) {
+        self.crucible.capture_prototype(prototype);
+    }
+}
+
+impl Application {
+    pub fn update(&mut self, window: &Window) {
+        self.gui.update();
+        let mut actions = self.gui.controls().take_actions();
+        if library_modified_timestamp() > self.library_modified && let Some(ref plan_name) = self.fabric_plan_name {
+            let fabric_plan = FabricPlan::load_preset(plan_name).expect("no such fabric plan");
+            actions.push(Action::BuildFabric(fabric_plan));
+            self.library_modified = library_modified_timestamp();
+        }
+        for action in actions {
+            match action {
+                Action::BuildFabric(fabric_plan) => {
+                    self.fabric_plan_name = Some(fabric_plan.name.clone());
+                    self.scene.show_surface(false);
+                    self.gui.queue_message(ControlMessage::Reset);
+                    self.crucible.build_fabric(fabric_plan);
+                }
+                Action::GravityChanged(_gravity) => {
+                    // TODO
+                }
+                Action::CalibrateStrain => {
+                    let strain_limits = self.crucible.strain_limits();
+                    self.gui.queue_message(ControlMessage::StrainThreshold(StrainThresholdMessage::SetStrainLimits(strain_limits)))
+                }
+                Action::SelectFace(face_id) => {
+                    self.scene.select_face(Some(face_id));
+                }
+                Action::AddBrick { face_alias, face_id } => {
+                    self.scene.select_face(None);
+                    self.crucible.add_brick(face_alias, face_id)
+                }
+                Action::ShowSurface => {
+                    self.scene.show_surface(true)
+                }
+            }
+        }
+        window.request_redraw();
+    }
+
+    pub fn redraw(&mut self, window: &Window) {
+        self.crucible.iterate();
+        if let Some(action) = self.crucible.action() {
+            self.gui.queue_message(ControlMessage::Action(action))
+        }
+        self.scene.update(&self.graphics, self.gui.controls().variation(self.scene.target_face_id()), self.crucible.fabric());
+        self.gui.update_viewport(&window);
+        match self.render() {
+            Ok(_) => {}
+            Err(wgpu::SurfaceError::Lost) => self.resize(self.graphics.size),
+            Err(wgpu::SurfaceError::OutOfMemory) => panic!("WGPU out of memory"),
+            Err(e) => eprintln!("{e:?}"),
+        }
+        let cursor_icon = self.gui.cursor_icon();
+        window.set_cursor_icon(cursor_icon);
+    }
+}
+
+impl Application {
+    pub fn handle_window_event(&mut self, event: &WindowEvent, window: &Window) {
+        self.gui.window_event(event, &window);
+        match event {
+            WindowEvent::Resized(physical_size) => self.resize(*physical_size),
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => self.resize(**new_inner_size),
+            WindowEvent::KeyboardInput { .. } => self.handle_keyboard_input(event),
+            WindowEvent::MouseInput { state: ElementState::Released, .. } => self.scene.window_event(event),
+
+            WindowEvent::MouseInput { .. } |
+            WindowEvent::CursorMoved { .. } |
+            WindowEvent::MouseWheel { .. }
+            if !self.gui.capturing_mouse() => self.scene.window_event(event),
+            _ => {}
+        }
+    }
+}
+
+impl Application {
+    pub fn new(graphics: GraphicsWindow, window: &Window) -> Application {
         let gui = GUI::new(&graphics, window);
         let scene = Scene::new(&graphics);
-        let crucible = Crucible::default();
         Application {
             graphics,
             scene,
             gui,
-            crucible,
+            crucible: Crucible::default(),
+            library_modified: library_modified_timestamp(),
+            fabric_plan_name: None,
         }
     }
 
@@ -74,130 +151,7 @@ impl Application {
         self.gui.post_render();
         Ok(())
     }
-}
 
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
-pub fn run_with(brick_index: Option<usize>) {
-    cfg_if::cfg_if! {
-        if #[cfg(target_arch = "wasm32")] {
-            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-            console_log::init_with_level(log::Level::Info).expect("Couldn't initialize logger");
-        } else {
-            env_logger::init();
-        }
-    }
-
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_inner_size(PhysicalSize::new(2048, 1600))
-        .build(&event_loop)
-        .expect("Could not build window");
-
-    window.set_title("Tensegrity Lab");
-    #[cfg(target_arch = "wasm32")]
-    {
-        use winit::platform::web::WindowExtWebSys;
-        web_sys::window()
-            .and_then(|win| {
-                let [width, height] = [win.inner_width(), win.inner_height()]
-                    .map(|x| x.unwrap().as_f64().unwrap() * 2.0);
-                window.set_inner_size(PhysicalSize::new(width, height));
-                let doc = win.document()?;
-                let dst = doc.get_element_by_id("body")?;
-                let canvas = web_sys::Element::from(window.canvas());
-                canvas.set_id("canvas");
-                dst.append_child(&canvas).ok()?;
-                Some(())
-            })
-            .expect("Couldn't append canvas to document body.");
-    }
-    let graphics = pollster::block_on(GraphicsWindow::new(&window));
-    let mut app = Application::new(graphics, &window);
-    if let Some(brick_index) = brick_index {
-        app.crucible.capture_prototype(brick_index); // TODO: should be a method on Application
-    }
-    // TODO: move these into Application
-    let mut library_modified = library_modified_timestamp();
-    let mut fabric_plan_name: Option<String> = None;
-
-    event_loop.run(move |event, _, control_flow| {
-        match event {
-            Event::WindowEvent { ref event, window_id } if window_id == window.id() => {
-                app.gui.window_event(event, &window);
-                match event {
-                    WindowEvent::CloseRequested { .. } => *control_flow = ControlFlow::Exit,
-                    WindowEvent::Resized(physical_size) => app.resize(*physical_size),
-                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => app.resize(**new_inner_size),
-                    WindowEvent::KeyboardInput { .. } => app.handle_keyboard_input(event),
-                    WindowEvent::MouseInput { state: ElementState::Released, .. } => app.scene.window_event(event),
-
-                    WindowEvent::MouseInput { .. } |
-                    WindowEvent::CursorMoved { .. } |
-                    WindowEvent::MouseWheel { .. }
-                    if !app.gui.capturing_mouse() => app.scene.window_event(event),
-
-                    _ => {}
-                }
-            }
-            Event::RedrawRequested(_) => {
-                app.crucible.iterate();
-                if let Some(action) = app.crucible.action() {
-                    app.gui.queue_message(ControlMessage::Action(action))
-                }
-                app.scene.update(&app.graphics, app.gui.controls().variation(app.scene.target_face_id()), app.crucible.fabric());
-                app.gui.update_viewport(&window);
-                match app.render() {
-                    Ok(_) => {}
-                    Err(wgpu::SurfaceError::Lost) => app.resize(app.graphics.size),
-                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                    Err(e) => eprintln!("{e:?}"),
-                }
-                window.set_cursor_icon(app.gui.cursor_icon());
-            }
-            Event::MainEventsCleared => {
-                app.gui.update();
-                let mut actions = app.gui.controls().take_actions();
-                if library_modified_timestamp() > library_modified && let Some(ref plan_name) = fabric_plan_name {
-                    let fabric_plan = FabricPlan::load_preset(plan_name).expect("no such fabric plan");
-                    actions.push(Action::BuildFabric(fabric_plan));
-                    library_modified = library_modified_timestamp();
-                }
-                for action in actions {
-                    match action {
-                        Action::BuildFabric(fabric_plan) => {
-                            fabric_plan_name = Some(fabric_plan.name.clone());
-                            app.scene.show_surface(false);
-                            app.gui.queue_message(ControlMessage::Reset);
-                            app.crucible.build_fabric(fabric_plan);
-                        }
-                        Action::GravityChanged(_gravity) => {
-                            // TODO
-                        }
-                        Action::CalibrateStrain => {
-                            let strain_limits = app.crucible.strain_limits();
-                            app.gui.queue_message(ControlMessage::StrainThreshold(StrainThresholdMessage::SetStrainLimits(strain_limits)))
-                        }
-                        Action::SelectFace(face_id) => {
-                            app.scene.select_face(Some(face_id));
-                        }
-                        Action::AddBrick { face_alias, face_id } => {
-                            app.scene.select_face(None);
-                            app.crucible.add_brick(face_alias, face_id)
-                        }
-                        Action::ShowSurface => {
-                            app.scene.show_surface(true)
-                        }
-                    }
-                }
-                window.request_redraw();
-            }
-            _ => {}
-        }
-    });
-}
-
-
-impl Application {
     fn handle_keyboard_input(&mut self, event: &WindowEvent) {
         let WindowEvent::KeyboardInput {
             input: KeyboardInput {
@@ -262,24 +216,4 @@ fn library_modified_timestamp() -> SystemTime {
         .unwrap()
         .modified()
         .unwrap()
-}
-
-fn fullscreen_web() {
-    #[cfg(target_arch = "wasm32")]
-    web_sys::window()
-        .and_then(|win| {
-            let document = win.document()?;
-            if document.fullscreen_element().is_none() {
-                let canvas = document.get_element_by_id("canvas")?;
-                match canvas.request_fullscreen() {
-                    Ok(_) => {}
-                    Err(e) => {
-                        info!("Could not request fullscreen: {e:?}");
-                    }
-                }
-            } else {
-                document.exit_fullscreen();
-            }
-            Some(())
-        });
 }
