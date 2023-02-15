d60ec065f6efa7cf95ee0ec4bd830a6aea7783a5
geralddejong <geralddejong@gmail.com>
2023-02-15 14:47:12 +0000

    tidying

--- src/build/tenscript/mod.rs
+++ src/build/tenscript/mod.rs
@@ -197,8 +197,8 @@ impl Library {
         let source = fs::read_to_string("src/build/tenscript/library.scm").unwrap();
         match Self::from_tenscript(&source) {
             Ok(library) => library,
-            Err(TenscriptError::Pest(error)) => panic!("pest parse error: \n{}", error),
-            Err(e) => panic!("{e:?}")
+            Err(TenscriptError::Pest(_error)) => panic!("pest parse error: \n{_error}"),
+            Err(_error) => panic!("{_error:?}")
         }
     }
 
