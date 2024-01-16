32edf5cb9fd5ec6bd21bb48cb90a4013d4c2a511
geralddejong <geralddejong@gmail.com>
2024-01-16 17:36:13 +0000

    tidying

--- src/graphics.rs
+++ src/graphics.rs
@@ -2,16 +2,16 @@ use wgpu::{BindGroupLayout, CommandEncoder, ShaderModule};
 use winit::window::Window;
 
 pub struct Graphics {
+    pub config: wgpu::SurfaceConfiguration,
     pub surface: wgpu::Surface,
     pub device: wgpu::Device,
     pub queue: wgpu::Queue,
-    pub config: wgpu::SurfaceConfiguration,
 }
 
 impl Graphics {
     pub async fn new(window: &Window) -> Self {
         let size = window.inner_size();
-        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor{
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
             backends: wgpu::Backends::all(),
             ..Default::default()
         });
@@ -26,22 +26,22 @@ impl Graphics {
             .expect("Could not request adapter");
 
         #[cfg(target_arch = "wasm32")]
-        let limits = wgpu::Limits::downlevel_webgl2_defaults()
+            let limits = wgpu::Limits::downlevel_webgl2_defaults()
             .using_resolution(adapter.limits());
 
         #[cfg(not(target_arch = "wasm32"))]
-        let limits = wgpu::Limits::default();
+            let limits = wgpu::Limits::default();
 
-        let (device, queue) = adapter.request_device(
-            &wgpu::DeviceDescriptor {
-                label: None,
-                features: wgpu::Features::empty(),
-                limits,
-            },
-            None,
-        )
-            .await
-            .expect("Could not request device");
+        let (device, queue) =
+            adapter
+                .request_device(
+                    &wgpu::DeviceDescriptor {
+                        label: None,
+                        features: wgpu::Features::empty(),
+                        limits,
+                    }, None)
+                .await
+                .expect("Could not request device");
         let surface_caps = surface.get_capabilities(&adapter);
         let surface_format = surface_caps.formats.iter()
             .copied().find(|f| f.is_srgb())
