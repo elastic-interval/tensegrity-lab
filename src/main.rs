b0ab35e78f4db8682dc742fddcb464f17ccb0fc6
Mitchell van der Hoeff <8631205+mitchelljustin@users.noreply.github.com>
2024-03-08 13:28:32 +0000

    fix web build

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 use clap::Parser;
 #[allow(unused_imports)]
 use log::info;
-use winit::{
-    event_loop::EventLoop,
-    window::WindowBuilder,
-};
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+use winit::{event_loop::EventLoop, window::WindowBuilder};
 use winit::dpi::PhysicalSize;
 use winit_input_helper::WinitInputHelper;
 
 use tensegrity_lab::application::Application;
 use tensegrity_lab::graphics::Graphics;
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::*;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -22,12 +19,11 @@ struct Args {
 }
 
 fn main() {
-    let Args { prototype } = Args::parse();
-    run(prototype);
+    run();
 }
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
-pub fn run(prototype: Option<usize>) {
+pub fn run() {
     cfg_if::cfg_if! {
         if #[cfg(target_arch = "wasm32")] {
             std::panic::set_hook(Box::new(console_error_panic_hook::hook));
@@ -38,49 +34,53 @@ pub fn run(prototype: Option<usize>) {
     }
 
     let event_loop = EventLoop::new().unwrap();
-    let window = WindowBuilder::new()
+    let winit_window = WindowBuilder::new()
         .with_inner_size(PhysicalSize::new(1600, 1200))
         .build(&event_loop)
         .expect("Could not build window");
 
-    window.set_title("Tensegrity Lab");
+    winit_window.set_title("Tensegrity Lab");
     #[cfg(target_arch = "wasm32")]
     {
         use winit::platform::web::WindowExtWebSys;
         web_sys::window()
-            .and_then(|win| {
-                let [width, height] = [win.inner_width(), win.inner_height()]
+            .and_then(|web_sys_window| {
+                let [width, height] = [web_sys_window.inner_width(), web_sys_window.inner_height()]
                     .map(|x| x.unwrap().as_f64().unwrap() * 2.0);
-                window.set_inner_size(PhysicalSize::new(width, height));
-                let doc = win.document()?;
+                info!("window swag {width} {height}");
+                winit_window.set_min_inner_size(Some(PhysicalSize::new(width, height)));
+                let doc = web_sys_window.document()?;
                 let dst = doc.get_element_by_id("body")?;
-                let canvas = web_sys::Element::from(window.canvas());
-                canvas.set_id("canvas");
-                dst.append_child(&canvas).ok()?;
+                let wgpu_canvas = winit_window.canvas()?;
+                let html_canvas = web_sys::Element::from(wgpu_canvas);
+                html_canvas.set_id("canvas");
+                dst.append_child(&html_canvas).ok()?;
                 Some(())
             })
             .expect("Couldn't append canvas to document body.");
     }
-    let graphics = pollster::block_on(Graphics::new(&window));
+    let graphics = pollster::block_on(Graphics::new(&winit_window));
     let mut app = Application::new(graphics);
     let mut input = WinitInputHelper::new();
-    if let Some(brick_index) = prototype {
+    if let Some(brick_index) = None {
         app.capture_prototype(brick_index);
     } else {
         let fabric = "Tommy Torque".to_string();
         app.run_fabric(&fabric)
     }
-    event_loop.run(move |event, window_target| {
-        if input.update(&event) {
-            if input.close_requested() {
-                window_target.exit();
-                return;
+    event_loop
+        .run(move |event, window_target| {
+            if input.update(&event) {
+                if input.close_requested() {
+                    window_target.exit();
+                    return;
+                }
+                app.handle_input(&input);
+                app.update();
+                app.redraw();
+            } else {
+                winit_window.request_redraw();
             }
-            app.handle_input(&input);
-            app.update();
-            app.redraw();
-        } else {
-            window.request_redraw();
-        }
-    }).unwrap();
+        })
+        .unwrap();
 }
