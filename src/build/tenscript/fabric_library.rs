29a6a59a24b61aa2f7679cddb305f3b72e65650b
Mitchell van der Hoeff <8631205+mitchelljustin@users.noreply.github.com>
2024-03-09 23:01:05 +0000

    my father i did it but you won't be proud of how i did it

--- src/build/tenscript/fabric_library.rs
+++ src/build/tenscript/fabric_library.rs
@@ -10,6 +10,11 @@ pub struct FabricLibrary {
 
 impl FabricLibrary {
     pub fn from_source() -> Result<Self, TenscriptError> {
+        let source = Self::load_source()?;
+        Self::from_tenscript(&source)
+    }
+
+    pub fn load_source() -> Result<String, TenscriptError> {
         let source: String;
         #[cfg(target_arch = "wasm32")]
         {
@@ -20,15 +25,45 @@ impl FabricLibrary {
             use std::fs;
             source = fs::read_to_string("fabric_library.scm").map_err(TenscriptError::FileRead)?;
         }
-        Self::from_tenscript(&source)
+        Ok(source)
+    }
+
+    pub fn load_specific_fabric_source(
+        name: Vec<String>,
+    ) -> Result<Option<String>, TenscriptError> {
+        let source = Self::load_source()?;
+        let pair = Self::parse_fabric_library_pair(&source)?;
+        for fabric_pair in pair.into_inner() {
+            let name_pair = fabric_pair
+                .clone()
+                .into_inner()
+                .find(|pair| pair.as_rule() == Rule::name)
+                .expect("no name pair");
+            let fabric_name: Vec<String> = name_pair
+                .into_inner()
+                .map(|pair| {
+                    let str_value = pair.as_str();
+                    str_value[1..str_value.len() - 1].to_string()
+                })
+                .collect();
+            if name == fabric_name {
+                return Ok(Some(fabric_pair.as_str().to_string()));
+            }
+        }
+        Ok(None)
     }
 
     fn from_tenscript(source: &str) -> Result<Self, TenscriptError> {
+        let pair = Self::parse_fabric_library_pair(source)?;
+        Self::from_pair(pair)
+    }
+
+    fn parse_fabric_library_pair(source: &str) -> Result<Pair<Rule>, TenscriptError> {
         let pair = TenscriptParser::parse(Rule::fabric_library, source)
             .map_err(TenscriptError::Pest)?
             .next()
-            .expect("no (fabrics ..)");
-        Self::from_pair(pair)
+            .expect("no (fabric-library ..)");
+        Ok(pair)
     }
 
     fn from_pair(pair: Pair<Rule>) -> Result<Self, TenscriptError> {
