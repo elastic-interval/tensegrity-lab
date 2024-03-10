b4130ea1cff2c90db254e987e22dc82cbb934ce6
Mitchell van der Hoeff <8631205+mitchelljustin@users.noreply.github.com>
2024-03-10 11:20:42 +0000

    rip out the janky ass editor

--- src/control_overlay/app.rs
+++ src/control_overlay/app.rs
@@ -1,18 +1,12 @@
 use std::sync::mpsc::Sender;
 
-use crate::build::tenscript::fabric_library::FabricLibrary;
 use leptos::{
-    component, create_effect, create_signal, event_target_value, view, IntoView, ReadSignal,
-    SignalGet, SignalSet,
+    component, create_signal, event_target_value, IntoView, ReadSignal, SignalGet,
+    SignalSet, view,
 };
-use leptos_use::storage::use_local_storage;
-use leptos_use::use_debounce_fn;
-use leptos_use::utils::FromToStringCodec;
 
 use crate::control_overlay::action::Action;
 use crate::fabric::interval::Interval;
-
-const SAVED_TENSCRIPT_KEY: &str = "SAVED_TENSCRIPT";
 
 #[component]
 pub fn ControlOverlayApp(
@@ -22,40 +16,12 @@ pub fn ControlOverlayApp(
     let pre_text = move || format!("{:#?}", control_state.get());
     let (category, set_category) = create_signal("Art".to_string());
     let (subname, set_subname) = create_signal("Halo by Crane".to_string());
-    let (tenscript, set_tenscript, _) =
-        use_local_storage::<String, FromToStringCodec>(SAVED_TENSCRIPT_KEY);
     let fabric_name = move || vec![category.get(), subname.get()];
     let on_run_fabric_click = move |_ev| {
         actions_tx
             .send(Action::LoadFabric(fabric_name()))
             .expect("failed to send action");
     };
-    let on_load_fabric_into_editor_click = move |_ev| {
-        let Ok(Some(fabric)) = FabricLibrary::load_specific_fabric_source(fabric_name()) else {
-            return;
-        };
-        set_tenscript.set(fabric.clone());
-        #[cfg(target_arch = "wasm32")]
-        {
-            let fabric_safe = fabric.replace('"', "\\\"").replace('`', "\\`");
-            let js = format!("window.AceEditor.setValue(`{fabric_safe}`, 1)");
-            js_sys::eval(&js).expect("JS failed to run");
-        }
-    };
-
-    let save_tenscript = use_debounce_fn(
-        move || {
-            #[cfg(target_arch = "wasm32")]
-            {
-                let new_tenscript = js_sys::eval("window.AceEditor.getValue()")
-                    .expect("could not get Ace editor content")
-                    .as_string()
-                    .expect("not a string");
-                set_tenscript.set(new_tenscript);
-            }
-        },
-        1000.,
-    );
     view! {
         <div class="inset">
             <section class="left">
@@ -73,23 +39,14 @@ pub fn ControlOverlayApp(
                     <button on:click=on_run_fabric_click>
                         Run Fabric
                     </button>
-                    <button on:click=on_load_fabric_into_editor_click>
-                        Load Fabric Into Editor
-                    </button>
                 </p>
                 <pre>{pre_text}</pre>
             </section>
             <section class="right">
-                <div id="tenscript_editor" on:change=move |_ev| { save_tenscript(); }>
-                    {move || tenscript.get()}
-                </div>
+                <h3>
+                    Right hand side content here.
+                </h3>
             </section>
-            <script>
-                let editor = ace.edit("tenscript_editor");
-                editor.setTheme("ace/theme/github_dark");
-                editor.session.setMode("ace/mode/scheme");
-                window.AceEditor = editor;
-            </script>
         </div>
     }
 }
