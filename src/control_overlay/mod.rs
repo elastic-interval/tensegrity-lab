0c23847da3e10a4c51e370deb21d014dbf4c999a
geralddejong <geralddejong@gmail.com>
2024-04-07 14:46:04 +0200

    Better halo

--- src/control_overlay/mod.rs
+++ src/control_overlay/mod.rs
@@ -2,3 +2,10 @@ pub mod action;
 pub mod overlay;
 
 pub use crate::control_state::ControlState;
+
+// export enum GlobalMode {
+//     Sphere = "sphere",
+//     Mobius = "mobius",
+//     Klein = "klein",
+//     Construction = "construction",
+// }
