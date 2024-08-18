7b20380a3902c815cb46d129ca7a917f85a19e62
geralddejong <geralddejong@gmail.com>
2024-08-18 19:51:09 +0200

    Navigating and picking revived! camera contents private

--- src/camera.rs
+++ src/camera.rs
@@ -6,8 +6,8 @@ use cgmath::{
 };
 use cgmath::num_traits::abs;
 use winit::dpi::PhysicalPosition;
-use winit::event::{ElementState, MouseButton, MouseScrollDelta, TouchPhase};
-use winit::keyboard::Key;
+use winit::event::{ElementState, MouseButton, MouseScrollDelta};
+use winit::event::MouseScrollDelta::PixelDelta;
 
 use crate::fabric::{Fabric, UniqueId};
 use crate::fabric::interval::Interval;
@@ -26,73 +26,86 @@ pub enum Pick {
 const TARGET_ATTRACTION: f32 = 0.01;
 
 pub struct Camera {
-    pub position: Point3<f32>,
-    pub target: Target,
-    pub look_at: Point3<f32>,
-    pub width: f32,
-    pub height: f32,
-    pub pick_cursor: Option<(f32, f32)>,
-    pub pick: Pick,
+    position: Point3<f32>,
+    target: Target,
+    look_at: Point3<f32>,
+    width: f32,
+    height: f32,
+    cursor_position: Option<PhysicalPosition<f64>>,
+    mouse_anchor: Option<PhysicalPosition<f64>>,
+    pick: Pick,
 }
 
 impl Camera {
-    pub fn new(
-        position: Point3<f32>,
-        width: f32,
-        height: f32,
-    ) -> Self {
+    pub fn new(position: Point3<f32>, width: f32, height: f32) -> Self {
         Self {
             position,
             target: Target::default(),
             look_at: point3(0.0, 3.0, 0.0),
             width,
             height,
-            pick_cursor: None,
+            cursor_position: None,
+            mouse_anchor: None,
             pick: Pick::Nothing,
         }
     }
 
+    pub fn set_target(&mut self, target: Target) {
+        self.target = target
+    }
+
+    pub fn current_pick(&self) -> &Pick {
+        &self.pick
+    }
+
+    pub fn set_pick(&mut self, pick: Pick) {
+        self.pick = pick;
+    }
+
     pub fn reset(&self) {
-        // get back to a good position 
+        // get back to a good position
     }
 
     pub fn cursor_moved(&mut self, position: PhysicalPosition<f64>) {
-        println!("Camera cursor moved {:?}", position)
+        self.cursor_position = Some(position);
+        if let Some(mouse_anchor) = self.mouse_anchor {
+            let diff = ((position.x - mouse_anchor.x) as f32, (position.y - mouse_anchor.y) as f32);
+            if let Some(rotation) = self.rotation(diff) {
+                self.position = self.look_at - rotation.transform_vector(self.look_at - self.position);
+            }
+            self.mouse_anchor = Some(position)
+        }
     }
 
-    pub fn mouse_wheel(&mut self, delta: MouseScrollDelta, phase: TouchPhase) {
-        println!("Camera mouse wheel {:?} {:?}", delta, phase)
+    pub fn mouse_wheel(&mut self, delta: MouseScrollDelta) {
+        if let PixelDelta(position) = delta {
+            let sy = position.y as f32;
+            let scroll = sy * SPEED.z;
+            let gaze = self.look_at - self.position;
+            if gaze.magnitude() - scroll > 1.0 {
+                self.position += gaze.normalize() * scroll;
+            }
+        }
     }
 
-    pub fn mouse_input(&mut self, state: ElementState, button: MouseButton) {
-        println!("Camera mouse input {:?} {:?}", state, button)
-    }
-
-    pub fn handle_input(&mut self, _input: &Key, _fabric: &Fabric) -> Option<Pick> {
-        // if input.mouse_held(MouseButton::Left) {
-        //     if let Some(rotation) = self.rotation(input.mouse_diff()) {
-        //         self.position = self.look_at - rotation.transform_vector(self.look_at - self.position);
-        //         self.pick_cursor = None;
-        //     }
-        // }
-        // if input.mouse_pressed(MouseButton::Left) {
-        //     self.pick_cursor = input.cursor();
-        // }
-        // if input.mouse_released(MouseButton::Left) {
-        //     if let Some(pick_cursor) = self.pick_cursor {
-        //         self.pick = self.pick(pick_cursor, fabric);
-        //         return Some(self.pick.clone());
-        //     }
-        // }
-        // let (_sx, sy) = input.scroll_diff();
-        // if abs(sy) > 0.1 {
-        //     let scroll = sy * SPEED.z;
-        //     let gaze = self.look_at - self.position;
-        //     if gaze.magnitude() - scroll > 1.0 {
-        //         self.position += gaze.normalize() * scroll;
-        //     }
-        // }
-        None
+    pub fn mouse_input(&mut self, state: ElementState, button: MouseButton, fabric: &Fabric) {
+        if button == MouseButton::Left {
+            match state {
+                ElementState::Pressed => {
+                    self.mouse_anchor = self.cursor_position
+                }
+                ElementState::Released => {
+                    if let (Some(anchor), Some(position)) = (self.mouse_anchor, self.cursor_position) {
+                        let (dx, dy) = ((position.x - anchor.x) as f32, (position.y - anchor.y) as f32);
+                        let diff = dx * dx + dy * dy;
+                        if diff < 32.0 {
+                            self.pick = self.pick((anchor.x as f32, anchor.y as f32), fabric);
+                        }
+                    }
+                    self.mouse_anchor = None
+                }
+            }
+        }
     }
 
     pub fn target_approach(&mut self, fabric: &Fabric) {
@@ -180,18 +193,18 @@ impl Camera {
         OPENGL_TO_WGPU_MATRIX * perspective(Rad(2.0 * PI / 5.0), aspect, 0.1, 100.0)
     }
 
-    fn _rotation(&self, (dx, dy): (f32, f32)) -> Option<Matrix4<f32>> {
+    fn rotation(&self, (dx, dy): (f32, f32)) -> Option<Matrix4<f32>> {
         if (dx, dy) == (0.0, 0.0) {
             return None;
         }
-        let rot_x = Matrix4::from_axis_angle(Vector3::unit_y(), Deg(dx * _SPEED.x));
+        let rot_x = Matrix4::from_axis_angle(Vector3::unit_y(), Deg(dx * SPEED.x));
         let axis = Vector3::unit_y().cross((self.look_at - self.position).normalize());
-        let rot_y = Matrix4::from_axis_angle(axis, Deg(dy * _SPEED.y));
+        let rot_y = Matrix4::from_axis_angle(axis, Deg(dy * SPEED.y));
         Some(rot_x * rot_y)
     }
 }
 
-const _SPEED: Vector3<f32> = vec3(-0.5, 0.4, 1.0);
+const SPEED: Vector3<f32> = vec3(-0.5, 0.4, 0.1);
 
 const OPENGL_TO_WGPU_MATRIX: Matrix4<f32> = Matrix4::new(
     1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.5, 1.0,
