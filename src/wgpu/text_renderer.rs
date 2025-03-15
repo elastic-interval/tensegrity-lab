d03f7b82c4baa3d6713480de09ccd3c0326f884b
Mitchell van der Hoeff <8631205+mitchelljustin@users.noreply.github.com>
2025-03-15 18:38:55 +0000

    it's nice innit guvnah

--- src/wgpu/text_renderer.rs
+++ src/wgpu/text_renderer.rs
@@ -1,12 +1,12 @@
 use crate::wgpu::Wgpu;
 use wgpu::{Device, RenderPass, SurfaceConfiguration};
 use wgpu_text::glyph_brush::ab_glyph::FontRef;
-use wgpu_text::glyph_brush::{HorizontalAlign, Layout, Section, Text, VerticalAlign};
+use wgpu_text::glyph_brush::{HorizontalAlign, Layout, OwnedSection, OwnedText, VerticalAlign};
 use wgpu_text::{BrushBuilder, TextBrush};
 
 pub struct TextRenderer {
     brush: TextBrush<FontRef<'static>>,
-    section: Section<'static>,
+    section: OwnedSection,
 }
 
 impl TextRenderer {
@@ -17,10 +17,10 @@ impl TextRenderer {
             BrushBuilder::using_font_bytes(include_bytes!("../../assets/Orbitron Bold.ttf"))
                 .unwrap()
                 .build(device, width, height, surface_configuration.format);
-        let section = Section::default()
+        let section = OwnedSection::default()
             .add_text(
-                Text::new("yoloswag")
-                    .with_color([1.0, 0.0, 0.0, 1.0])
+                OwnedText::new("Building..")
+                    .with_color([0.8, 0.8, 0.8, 1.0])
                     .with_scale(40.0),
             )
             .with_layout(
@@ -28,9 +28,13 @@ impl TextRenderer {
                     .v_align(VerticalAlign::Center)
                     .h_align(HorizontalAlign::Center),
             )
-            .with_bounds([width as f32, height as f32])
-            .with_screen_position([width as f32 / 2.0, height as f32 / 2.0]);
+            .with_bounds([width as f32 / 2.0, 300.0])
+            .with_screen_position([width as f32 / 2.0, 100.0]);
         TextRenderer { brush, section }
+    }
+    
+    pub fn update(&mut self, new_text: String) {
+        self.section.text[0].text = new_text;
     }
 
     pub fn draw<'a>(&'a mut self, render_pass: &mut RenderPass<'a>, wgpu: &Wgpu) {
