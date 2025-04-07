932951f0ab302b59e2887c98e8c5510128d3179e
geralddejong <geralddejong@gmail.com>
2025-04-07 13:57:11 +0200

    Physics parameter adjustment

--- src/fabric/physics.rs
+++ src/fabric/physics.rs
@@ -2,6 +2,7 @@
  * Copyright (c) 2020. Beautiful Code BV, Rotterdam, Netherlands
  * Licensed under GNU GENERAL PUBLIC LICENSE Version 3.
  */
+use crate::messages::{ParameterType, PhysicsFeature, PhysicsParameter};
 
 #[derive(Debug, Clone, Copy, Default, PartialEq)]
 pub enum SurfaceCharacter {
@@ -21,6 +22,50 @@ pub struct Physics {
     pub drag: f32,
     pub stiffness: f32,
     pub muscle_nuance_increment: f32,
+}
+
+impl Physics {
+    pub fn accept(&mut self, parameter: PhysicsParameter) {
+        use PhysicsFeature::*;
+        let PhysicsParameter {
+            value,
+            parameter_type,
+            feature,
+        } = parameter;
+        match feature {
+            Gravity => match parameter_type {
+                ParameterType::Report => {}
+                ParameterType::Set => self.gravity = value,
+                ParameterType::Adjust => self.gravity *= value,
+            },
+            Pretense => {
+                unimplemented!()
+            }
+            Stiffness => match parameter_type {
+                ParameterType::Report => {}
+                ParameterType::Set => self.stiffness = value,
+                ParameterType::Adjust => self.stiffness *= value,
+            },
+            IterationsPerFrame => {
+                unimplemented!()
+            }
+            MuscleIncrement => match parameter_type {
+                ParameterType::Report => {}
+                ParameterType::Set => self.muscle_nuance_increment = value,
+                ParameterType::Adjust => self.muscle_nuance_increment *= value,
+            },
+            Viscosity => match parameter_type {
+                ParameterType::Report => {}
+                ParameterType::Set => self.viscosity = value,
+                ParameterType::Adjust => self.viscosity *= value,
+            },
+            Drag => match parameter_type {
+                ParameterType::Report => {}
+                ParameterType::Set => self.drag = value,
+                ParameterType::Adjust => self.drag *= value,
+            },
+        }
+    }
 }
 
 pub mod presets {
