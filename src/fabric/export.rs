df9e7a2deb915b8d1c023426643f8635c0dbc47b
geralddejong <geralddejong@gmail.com>
2025-04-10 18:47:55 +0200

    more drag, export fixed

--- src/fabric/export.rs
+++ src/fabric/export.rs
@@ -45,7 +45,7 @@ impl Fabric {
             .filter(|(_, Joint { fixed, .. })| !fixed)
         {
             let Point3 { x, y, z } = location * self.scale;
-            writeln!(zip, "{};{x:.4};{y:.4};{z:.4}", index + 1)?;
+            writeln!(zip, "{};{x:.2};{y:.2};{z:.2}", index + 1)?;
         }
 
         // Create intervals CSV
@@ -62,7 +62,7 @@ impl Fabric {
             } = interval;
             writeln!(
                 zip,
-                "=\"{},{}\";{role:?};{length:.4};{strain:.4}",
+                "=\"{},{}\";{role:?};{length:.2};{strain:.10}",
                 alpha_index + 1,
                 omega_index + 1
             )?;
@@ -73,7 +73,6 @@ impl Fabric {
         let submerged = self
             .joints
             .iter()
-            .filter(|Joint { fixed, .. }| !fixed)
             .enumerate()
             .filter(
                 |(
@@ -92,61 +91,5 @@ impl Fabric {
         // Finalize the ZIP file
         let buffer = zip.finish()?.into_inner();
         Ok(buffer)
-    }
-
-    pub fn csv(&self) -> String {
-        let joints = self
-            .joints
-            .iter()
-            .filter(|Joint { fixed, .. }| !fixed)
-            .enumerate()
-            .map(|(index, Joint { location, .. })| {
-                let Point3 { x, y, z } = location * self.scale;
-                // ["index", "x", "y", "z"]
-                format!("{};{x:.4};{y:.4};{z:.4}", index + 1)
-            })
-            .join("\n");
-        let intervals = self
-            .interval_values()
-            .map(|interval| {
-                let length = interval.length(&self.joints) * self.scale;
-                let strain = interval.strain;
-                let role = interval_material(interval.material).role;
-                let Interval {
-                    alpha_index,
-                    omega_index,
-                    ..
-                } = interval;
-                // ["joints", "role", "length", "strain"]
-                format!(
-                    "=\"{},{}\";{role:?};{length:.4};{strain:.4}",
-                    alpha_index + 1,
-                    omega_index + 1
-                )
-            })
-            .join("\n");
-        let submerged = self
-            .joints
-            .iter()
-            .filter(|Joint { fixed, .. }| !fixed)
-            .enumerate()
-            .filter(
-                |(
-                    _,
-                    Joint {
-                        location: Point3 { y, .. },
-                        ..
-                    },
-                )| *y <= 0.0,
-            )
-            .map(|(index, _)| index + 1)
-            .join(",");
-        let height = self
-            .joints
-            .iter()
-            .fold(0.0f32, |h, joint| h.max(joint.location.y))
-            * self.scale;
-        let scale = self.scale;
-        format!("Height: {height:.1} Scale: {scale:.1}\n\nIndex;X;Y;Z\n{joints}\n\nJoints;Role;Length;Strain\n{intervals}\n\nSubmerged\n=\"{submerged}\"")
     }
 }
