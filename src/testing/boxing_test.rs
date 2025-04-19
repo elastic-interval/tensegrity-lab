b405cfb72fe9022efe296162e21442d65a06cfab
geralddejong <geralddejong@gmail.com>
2025-04-19 15:53:22 +0200

    two boxing steps

--- src/testing/boxing_test.rs
+++ src/testing/boxing_test.rs
@@ -1,18 +1,24 @@
+use std::collections::VecDeque;
+use crate::fabric::material::Material;
 use crate::fabric::physics::{Physics, SurfaceCharacter};
 use crate::fabric::Fabric;
 use crate::{Age, PhysicsFeature, TesterAction};
+use cgmath::Point3;
 use itertools::Itertools;
 
 pub struct BoxingTest {
     pub fabric: Fabric,
     pub physics: Physics,
-    steps: Vec<TimedStep>,
+    steps: VecDeque<BoxingStep>,
 }
 
 impl BoxingTest {
     pub fn new(fabric: &Fabric, physics: Physics) -> Self {
         let fabric = fabric.clone();
-        let steps = vec![Self::remove_support()];
+        let steps = VecDeque::from([
+            BoxingStep::RemoveSupport,
+            BoxingStep::Deflate,
+        ]);
         Self {
             fabric,
             physics,
@@ -22,12 +28,11 @@ impl BoxingTest {
 
     pub fn iterate(&mut self) {
         self.fabric.iterate(&self.physics);
-        if let Some(step) = self.steps.first() {
-            if self.fabric.age.within(&step.age) {
-                return;
+        if let Some(step) = self.steps.front() {
+            if !self.fabric.age.within(&step.age()) {
+                step.execute(&mut self.fabric, &mut self.physics);
+                self.steps.pop_front();
             }
-            (step.execute)(&mut self.fabric, &mut self.physics);
-            self.steps.remove(0);
         }
     }
 
@@ -46,29 +51,53 @@ impl BoxingTest {
             _ => {}
         }
     }
+}
 
-    fn remove_support() -> TimedStep {
-        TimedStep {
-            age: Age::seconds(40),
-            execute: Box::new(|fabric, physics| {
-                physics.surface_character = SurfaceCharacter::Bouncy;
-                let supports = fabric
-                    .intervals
-                    .iter()
-                    .filter_map(|(id, interval)| {
-                        interval.material.properties().support.then_some(id)
-                    })
-                    .cloned()
-                    .collect_vec();
-                for support in supports {
-                    fabric.remove_interval(support)
+enum BoxingStep {
+    RemoveSupport,
+    Deflate,
+}
+
+impl BoxingStep {
+    fn age(&self) -> Age {
+        match self {
+            BoxingStep::RemoveSupport => Age::seconds(38),
+            BoxingStep::Deflate => Age::seconds(50),
+        }
+    }
+
+    fn execute(&self, fabric: &mut Fabric, physics: &mut Physics) {
+        match self {
+            BoxingStep::RemoveSupport => {
+                remove_supports(fabric);
+                for joint in &mut fabric.joints {
+                    joint.fixed = false;
                 }
-            }),
+                let base = fabric.create_fixed_joint(Point3::new(10.0, 0.0, 0.0));
+                let length = 10.0;
+                fabric.create_interval(3, base, length, Material::GuyLine);
+                fabric.progress.start(20000);
+                physics.surface_character = SurfaceCharacter::Sticky;
+            },
+            BoxingStep::Deflate => {
+                remove_supports(fabric);
+                physics.pretenst = -10.0;
+                fabric.set_pretenst(physics.pretenst, 20000);
+            },
         }
     }
 }
 
-struct TimedStep {
-    age: Age,
-    execute: Box<dyn Fn(&mut Fabric, &mut Physics) -> ()>,
+fn remove_supports(fabric: &mut Fabric) {
+    fabric
+        .intervals
+        .iter()
+        .filter_map(|(id, interval)| interval.material.properties().support.then_some(id))
+        .cloned()
+        .collect_vec()
+        .into_iter()
+        .for_each(|support| {
+            let omega = fabric.remove_interval(support).omega_index;
+            fabric.remove_joint(omega);
+        });
 }
